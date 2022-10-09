fd4d41a0aa7fbc50154a6a59fe3bbd020214d808
Tom <153046sk@gmail.com>
2022-10-09 15:55:14 +0900

    fix login simple

--- oneon/oneon/app/Http/Controllers/Auth/LoginController.php
+++ oneon/oneon/app/Http/Controllers/Auth/LoginController.php
@@ -40,38 +40,4 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
-    public function init() {
-        return view('pages.login.login', []);
-    }
-
-    public function signin(Request $request) {
-
-        $oneonId = $request->oneonid;
-        $password = $request->password;
-        
-        $query =  DB::table('t_employees')
-            ->where('t_employees.oneon_id', $oneonId)
-            ->select(DB::raw('
-                t_employees.oneon_id,
-                concat( t_employees.last_name, " ", t_employees.first_name) as full_name,
-                t_employees.mentee_times,
-                t_employees.mentor_times,
-                t_employee_tags.tag_code,
-                t_employee_tags.department_current_code,
-                t_employee_tags.department_past_code,
-                t_employee_tags.job_current_code,
-                t_employee_tags.job_past_code
-            '))
-            ->where('oneon_id', $oneonId)
-            ->where('mail_address_temporary', $password)
-            ->first();
-
-        if (empty($query)) {
-            ession(['oneonId' => $oneonId]);
-            return redirect()->route('home');
-        }
-
-        return redirect()->back();
-    }
 }
